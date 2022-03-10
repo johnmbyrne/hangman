8379fb16e3af8dbb2e7fef956265efaea9f0401d
johnmbyrne <john.byrne@corvusconsulting.eu>
2022-03-10 21:02:06 +0000

    Enter comments to explain what the code is doing

--- run.py
+++ run.py
@@ -4,22 +4,24 @@ def hangman():
     """
     Classic Hangman game function
     """
-
-
-    word = random.choice(["football","tennis","rugby","basketball","snooker","hurling","squash"])
-    letters_choice = 'qwertyuiopasdfghjklzxcvbnm'
-    guesses = 12
+    word = random.choice(["football","tennis","rugby","basketball","snooker","hurling","squash"])  # The word list the game will choose from
+    letters_choice = 'qwertyuiopasdfghjklzxcvbnm'  # The valid inputs the user can select from
+    guesses = 12  # The maximum number of times the game will loop if the word is not guessed
     guess_made = ''
 
+    # Main gameplay section
     while len(word) > 0:
         answer = ''
         wrong = 0
 
+        # What the game will do with the letter guesses
         for letter in word:
             if letter in guess_made:
                 answer = answer + letter
             else:
                 answer = answer + '_'+''
+        
+        # If the word is correctly guessed
         if answer == word:
             print(answer)
             print("Congratulations, you're a Winner!")
@@ -27,6 +29,7 @@ def hangman():
         print("Guess the word:", answer)
         guess = input()
 
+        # To make sure only valid letters are picked
         if guess in letters_choice:
             guess_made = guess_made + guess
         else:
