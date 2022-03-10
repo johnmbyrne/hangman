6cd74403a317962e6ef2ad2ea8de725a82b07818
johnmbyrne <john.byrne@corvusconsulting.eu>
2022-03-10 20:46:17 +0000

    Create main Hangman game function

--- run.py
+++ run.py
@@ -1,3 +1,36 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def hangman():
+    """
+    Classic Hangman game function
+    """
+
+
+    word = random.choice(["football","tennis","rugby","basketball","snooker","hurling","squash"])
+    letters_choice = 'qwertyuiopasdfghjklzxcvbnm'
+    guesses = 12
+    guess_made = ''
+
+    while len(word) > 0:
+        answer = ''
+        wrong = 0
+
+        for letter in word:
+            if letter in guess_made:
+                answer = answer + letter
+            else:
+                answer = answer + '_'+''
+        if answer == word:
+            print(answer)
+            print("Congratulations, you're a Winner!")
+        
+        print("Guess the word:", answer)
+        guess = input()
+
+        if guess in letters_choice:
+            guess_made = guess_made + guess
+        else:
+            print("Enter a letter from a-z:")
+            guess = input()
+
+hangman()        
