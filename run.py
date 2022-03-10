498825886d51203d4850fe1f5430ba59762d70b4
johnmbyrne <john.byrne@corvusconsulting.eu>
2022-03-10 21:30:27 +0000

    Complete first draft of game

--- run.py
+++ run.py
@@ -35,5 +35,75 @@ def hangman():
         else:
             print("Enter a letter from a-z:")
             guess = input()
+        
+        # What happens when wrong guess is made
+        if guess not in word:
+            guesses = guesses - 1
+            if guesses == 11:
+                print("11 guesses left")
+                print("I I I I")
+                print("I I I I")
+                print("I I I X")
+            if guesses == 10:
+                print("10 guesses left")
+                print("I I I I")
+                print("I I I I")
+                print("I I X X")
+            if guesses == 9:
+                print("9 guesses left")
+                print("I I I I")
+                print("I I I I")
+                print("I X X X")
+            if guesses == 8:
+                print("8 guesses left")
+                print("I I I I")
+                print("I I I I")
+                print("X X X X")
+            if guesses == 7:
+                print("7 guesses left")
+                print("I I I I")
+                print("I I I X")
+                print("X X X X")
+            if guesses == 6:
+                print("6 guesses left")
+                print("I I I I")
+                print("I I X X")
+                print("X X X X")
+            if guesses == 5:
+                print("5 guesses left")
+                print("I I I I")
+                print("I X X X")
+                print("X X X X")
+            if guesses == 4:
+                print("4 guesses left")
+                print("I I I I")
+                print("X X X X")
+                print("X X X X")
+            if guesses == 3:
+                print("3 guesses left")
+                print("I I I X")
+                print("X X X X")
+                print("X X X X")
+            if guesses == 2:
+                print("2 guesses left")
+                print("I I X X")
+                print("X X X X")
+                print("X X X X")
+            if guesses == 1:
+                print("1 guess left")
+                print("I X X X")
+                print("X X X X")
+                print("X X X X")
+            if guesses == 0:
+                print("Nope, you lost!")
+                print("The correct answer was ", word)
+                print("X X X X")
+                print("X X X X")
+                print("X X X X")
 
-hangman()        
+# The initial introduction
+name = input("Enter your name")
+print("Hi ", name ," let's play Hangman!\n")
+print("Guess the word, but you'll only get 12 wrong attempts!\n")
+hangman()
+print()
